0af5ee5fb8e0042a7f9b27ce221d4455aae59f26
hkhoi <hkhoi@outlook.com>
2016-09-09 17:10:15 +0900

    update

--- ctci/src/chap9_dp_recursion/ToDo.java
+++ ctci/src/chap9_dp_recursion/ToDo.java
@@ -28,5 +28,96 @@ public class ToDo {
         return 0;
     }
     
+    /**
+     * a[i] = i -> magic
+     * Given a sorted array of distinct integers.
+     * @param array
+     * @return 
+     */
+    public boolean containsMagicIndex(int[] array) {
+        return false;
+    }
     
+    /**
+     * a[i] = i -> magic
+     * Given a sorted array of non-distinct integers.
+     * @param array
+     * @return 
+     */
+    public boolean containsMagicIndex2(int[] array) {
+        return false;
+    }
+    
+    /**
+     * Print all sub-sets
+     * @param set 
+     */
+    public void printSuperSet(int[] set) {
+        
+    }
+    
+    /**
+     * Generate all permutations of an array.
+     * @param set 
+     */
+    public void genAllPerm(int[] set) {
+        
+    }
+    
+    /**
+     * You knew this problem
+     * @param n 
+     */
+    public void allPairs(int n) {
+        
+    }
+    
+    /**
+     * You know this shit. Paint simulator 2.0
+     * @param canvas
+     * @param x
+     * @param y 
+     */
+    public void paint(int[][] canvas, int x, int y) {
+        
+    }
+    
+    /**
+     * 25, 10, 5, 1
+     * @param cents
+     * @return 
+     */
+    public long moneyExchange(int cents) {
+        return 0;
+    }
+    
+    /**
+     * You know what to do
+     * @param n 
+     */
+    public void nQueen(int n) {
+        
+    }
+    
+    /**
+     * n boxes with w, h, and d, stack them, the box below must be large than the above
+     * in 3 measurement.
+     * Find the highest shit
+     * @param w
+     * @param h
+     * @param d
+     * @return 
+     */
+    public long stackBox(int[] w, int[] h, int[] d) {
+        return 0;
+    }
+    
+    /**
+     * Give shit like this: 1|0&4
+     * @param expression
+     * @param value 
+     */
+    public void parathesizing(String expression, boolean value) {
+        
+    }
 }
