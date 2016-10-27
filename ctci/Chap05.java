2c33ef7ea94815449736bcc9412f8b731e41cd6b
hkhoi <hkhoi@outlook.com>
2016-10-27 08:27:45 +0700

    Finish first iteration for Chap 05

--- ctci/Chap05.java
+++ ctci/Chap05.java
@@ -3,9 +3,12 @@ import java.util.*;
 public class Chap05 {
 
 	public static void main(String[] args) {
-		System.out.println(bitDif(31, 14));
+		System.out.println(bitDif(15, 14));
 	}
 
+	/**
+	 * Returns the number of bit to make a become b
+	 */
 	public static int bitDif(int a, int b) {
 		int xor = a ^ b;
 		int counter = 0;
@@ -17,5 +20,12 @@ public class Chap05 {
 		}
 
 		return counter;
+	}
+
+	/**
+	 * Swap bit 0, 1; 2, 3; 4,5...
+	 * With as few instructions as possible
+	 */
+	public static int swapBit(int n) {
 	}
 }
