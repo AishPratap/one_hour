414cc3ec381fe71f0be340dc1018efec7faf20b9
hkhoi <hkhoi@outlook.com>
2016-10-27 09:09:07 +0700

    Tic tac toe judge

--- ctci/Chap17.java
+++ ctci/Chap17.java
@@ -3,9 +3,6 @@ import java.util.*;
 public class Chap17 {
 
 	public static void main(String[] args) {
-		int[] data = {490, 99};
-		swap0(data);
-		System.out.println(Arrays.toString(data));
 	}
 
 	/**
@@ -22,5 +19,49 @@ public class Chap17 {
 		data[0] ^= data[1];
 		data[1] ^= data[0];
 		data[0] ^= data[1];
+	}
+
+	/**
+	 * Decide who won the game of tic tac toe
+	 * Assume that the board 3x3 is fixed
+	 * 1: O, X: -1: Nope: 0
+	 * Time: O(1)
+	 */
+	public static int ticTacToe(int[][] board) {
+		for (int i = 0; i < 3; ++i) {
+			if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+				if (board[i][0] == 1) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+
+			if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+				if (board[0][i] == 1) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+		}
+
+		if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+			if (board[1][1] == 1) {
+				return 1;
+			} else {
+				return -1;
+			}
+		}
+
+		if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+			if (board[1][1] == 1) {
+				return 1;
+			} else {
+				return -1;
+			}
+		}
+
+		return 0;
 	}
 }
