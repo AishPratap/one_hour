bc2cfc1daf27556e2826acc74ae3e7cf38387e33
hkhoi <hkhoi@outlook.com>
2016-10-28 16:44:19 +0700

    Add without arithmetic operators

--- ctci/Chap18.java
+++ ctci/Chap18.java
@@ -3,23 +3,27 @@ import java.util.*;
 public class Chap18 {
 
 	public static void main(String[] args) {
-		System.out.println(add(3, 5));
+		System.out.println(add(85, 25));
 	}
 
+	/**
+	 * Add two positive integers without addition operator
+	 */
 	public static int add(int a, int b) {
-		boolean carry = false;
 		int result = 0;
+		int mask = 1;
 
-		while (a > 0 || b > 0) {
+		boolean carry = false;
+
+		while (a != 0 || b != 0) {
 			int bitA = a & 1;
 			int bitB = b & 1;
-
-			System.out.printf("Bit A: %d, Bit B: %d\n", bitA, bitB);
 
 			int curBit = 0;
 
 			if ((bitA & bitB) == 1) {
 				curBit = 0;
+
 				if (carry) {
 					curBit = 1;
 				}
@@ -27,27 +31,32 @@ public class Chap18 {
 				carry = true;
 			} else if ((bitA | bitB) == 1) {
 				curBit = 1;
+
 				if (carry) {
 					curBit = 0;
 				}
 			} else {
 				curBit = 0;
+				
 				if (carry) {
 					curBit = 1;
 					carry = false;
 				}
 			}
 
+			if (curBit == 1) {
+				result |= mask;
+			}
+
 			a >>= 1;
 			b >>= 1;
-
-			System.out.println("DEBUG:curBit: " + curBit);
-			result |= curBit;
-
-			result <<= 1;
+			mask <<= 1;
 		}
 
-		
+		if (carry) {
+			result |= mask;
+		}
+
 		return result;
 	}
 }
