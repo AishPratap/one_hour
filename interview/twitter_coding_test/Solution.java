950969cc4108e1542b51490db98e29fc9db905e7
hkhoi <hkhoi@outlook.com>
2016-11-02 08:35:36 +0700

    Bugs not resolved yet

--- interview/twitter_coding_test/Solution.java
+++ interview/twitter_coding_test/Solution.java
@@ -3,12 +3,87 @@ import java.util.*;
 
 public class Solution {
 	public static void main(String[] args) {
-		String mes =
-			"Otjfvknou kskgnl, K mbxg iurtsvcnb ksgq hoz atv. Vje xcxtyqrl vt ujg smewfv vrmcxvtg rwqr ju vhm ytsf elwepuqyez. -Atvt hrqgse, Cnikg";
-		String sig = "-Atvt :) hrqgse, Cnikg!!";
-		String test = "Otjfvknou kskgnl";
-		System.out.println(decrypt(mes));
+		//String mes =
+		//"Otjfvknou kskgnl, K mbxg iurtsvcnb ksgq hoz atv. Vje xcxtyqrl vt ujg smewfv vrmcxvtg rwqr ju vhm ytsf elwepuqyez. -Atvt hrqgse, Cnikg";
+		//String sig = "-Atvt :) hrqgse, Cnikg!!";
+		//String test = "Otjfvknou kskgnl";
+		//System.out.println(decrypt(mes));
+		//
+		//String seq = "AATTGGCC";
+		String[] bank = {
+			"GAAAAAAA",
+			"AAGAAAAA",
+			"AAAAGAAA",
+			"GGAAAAAA",
+		};
+
+		int a = findMutationDistance("AAAAAAAA", "GGAAAAAA", bank);
+		System.out.println(a);
 	}
+
+    public static int findMutationDistance(String start, String end, String[] bank) {
+        /* Load data to HashSet  */
+        Map<String, Integer> path = new HashMap<>();
+        for (String word : bank) {
+            path.put(word, -1);
+        }
+        
+        //System.out.println("DEBUG:bankSet: " + dataSet);
+        
+        /* If the end sequence is not there  */
+        if (!path.containsKey(end)) {
+            return -1;
+        }
+        
+        /* BFS */
+        Queue<String> queue = new LinkedList<>();
+        int counter = -1;
+        
+        queue.add(start);
+        path.put(start, 0);
+        
+        while (!queue.isEmpty()) {
+            String current = queue.poll();
+	    //System.out.println("DEBUG:current: " + current);
+	    int pathSoFar = path.get(current);
+	    path.remove(current);
+
+            if (current.equals(end)) {
+		return pathSoFar;
+            }
+            
+            List<String> next = nextWords(current, path);
+	     //System.out.println("DEBUG:next: " + next);
+            for (String word : next) {
+		path.put(word, pathSoFar + 1);
+                queue.add(word);
+            }
+        }
+        
+        return -1;
+    }
+    
+    public static List<String> nextWords(String current, Map<String, Integer> path) {
+        char[] tokens = {'A', 'C', 'T', 'G'};
+        List<String> result = new LinkedList<>();
+        
+        for (int i = 0; i < current.length(); ++i) {
+            char[] array = current.toCharArray();
+            for (char c : tokens) {
+                if (c == array[i]) {
+                    continue;
+                }
+                array[i] = c;
+                String str = new String(array);
+                if (path.containsKey(str)) {
+                    result.add(str);
+                }
+            }
+        }
+        
+        //System.out.println("DEBUG:path: " + path);
+        return result;
+    }
 
 	public static String decrypt(String encrypted_message) {
 		int[] key= {8, 2, 5, 1, 2, 2, 0};
@@ -60,137 +135,6 @@ public class Solution {
 		}
 
 		return d;
-	}
-
-	static int findMutationDistance(String start, String end, String[] bank) {
-		DNAGraph graph = new DNAGraph(bank);
-		return graph.getDistance(start, end);
-	}
-
-	
-	static  boolean isMutation(String strA, String strB) {
-		int dif = 0;
-		for (int i = 0; i < strA.length(); ++i) {
-			if (strA.charAt(i) != strB.charAt(i)) {
-				dif += 1;
-			}
-		}
-
-		return dif == 1;
-	}
-
-	static class DNAGraph {
-
-		public List<List<Integer>> graph;
-		public Map<String, Integer> dnaMap;
-		public String[] bank;
-		public int size;
-
-		public DNAGraph(String[] bank) {
-			size = bank.length;
-			this.bank = bank;
-
-			dnaMap = new HashMap<>();
-			for (int i = 0; i < size; ++i) {
-				dnaMap.put(bank[i], i);
-			}
-
-			graph = new ArrayList<>();
-			for (int i= 0; i < size; ++i) {
-				graph.add(new ArrayList<>());
-			}
-
-			for (int i = 0; i < size; ++i) {
-				for (int j = i + 1; j < size; ++j) {
-					if (isMutation(bank[i], bank[j])) {
-						connect(i, j);
-					}
-				}
-			}
-		}
-
-		private void connect(int a, int b) {
-			graph.get(a).add(b);
-			graph.get(b).add(a);
-		}
-
-		private boolean isMutation(String strA, String strB) {
-			int dif = 0;
-			for (int i = 0; i < strA.length(); ++i) {
-				if (strA.charAt(i) != strB.charAt(i)) {
-					dif += 1;
-				}
-			}
-
-			return dif == 1;
-		}
-
-		private int getDistance(int a, int b) {
-			int dis = -1;
-
-			Queue<Integer> queue = new LinkedList<>();
-			boolean[] visited = new boolean[size];
-
-			queue.add(a);
-
-			while (!queue.isEmpty()) {
-				int cur = queue.poll();
-				visited[cur] = true;
-
-				dis += 1;
-
-				if (cur == b) {
-					return dis;
-				}
-
-				for (int it : graph.get(cur)) {
-					if (!visited[it]) {
-						queue.add(it);
-					}
-				}
-			}
-
-			return -1;
-		}
-
-		public int getDistance(String strA, String strB) {
-			if (!dnaMap.containsKey(strB)) {
-				return -1;
-			}
-
-			int b = dnaMap.get(strB);
-
-			if (!dnaMap.containsKey(strA)) {
-				int minDis = -1;
-
-				for (int i = 0; i < size; ++i) {
-					if (isMutation(strA, bank[i])) {
-						int temp = getDistance(i, b) + 1;
-						if (temp > 0) {
-							if (minDis == -1) {
-								minDis = temp;
-							} else {
-								minDis = Math.min(minDis, temp);
-							}
-						}
-					}
-					if (minDis == 1) {
-						return 1;
-					}
-				}
-				return minDis;
-			}
-
-			int a = dnaMap.get(strA);
-
-			return getDistance(a, b);
-		}
-
-		//public void dump() {
-		//for (List<Integer> it : graph) {
-		//System.out.println(it);
-		//}
-		//}
 	}
 }
 
