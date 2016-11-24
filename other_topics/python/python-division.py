fc153c5e5ca7b8df5b6fa4e83358790b98eb131b
hkhoi <hkhoi@outlook.com>
2016-11-24 22:26:04 +0700

    push

---
+++ other_topics/python/python-division.py
@@ -0,0 +1,6 @@
+a = float(raw_input())
+b = float(raw_input())
+
+print int(a / b)
+print float(a / b)
+
--- other_topics/python/unpack.py
+++
@@ -1,3 +0,0 @@
-pack = ('hoang', 'khoi', 'dep', 'trai')
-hoang, khoi, dep, trai = pack
-print hoang
