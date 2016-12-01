c300224910691f576504c4ad50bae582a304fd3f
hkhoi <hkhoi@outlook.com>
2016-12-01 23:51:49 +0700

    Queue API

---
+++ other_topics/java/MyQueue.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Important methods to remember
+ *	poll()
+ *	add()
+ *	peek()
+ */
+public class MyQueue {
+
+	public static void main(String[] args) {
+		Queue<Integer> queue = new LinkedList<>();
+		for (int i = 0; i < 10; ++i) {
+			queue.add(i);
+		}
+
+		while (!queue.isEmpty()) {
+			System.out.println(queue.poll());
+		}
+	}
+}
