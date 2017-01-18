be7b068ea7f54b15fe6d629934b023fece363602
hkhoi <hkhoi@outlook.com>
2017-01-18 23:27:35 +0700

    tempalte for cp

---
+++ hardcore_training/my_compro/02_array/00230.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: Khoi Hoang
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+}
