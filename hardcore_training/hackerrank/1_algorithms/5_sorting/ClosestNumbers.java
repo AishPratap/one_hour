90dd6518449616edb56d317b67ea65670e57da17
hkhoi <hkhoi@outlook.com>
2017-01-21 22:24:30 +0700

    Sorting

---
+++ hardcore_training/hackerrank/1_algorithms/5_sorting/ClosestNumbers.java
@@ -0,0 +1,122 @@
+/*
+ * hoang_khoi
+ */
+import java.io.*;
+import java.util.*;
+
+public class ClosestNumbers {
+
+	public static void main(String[] args) throws Exception {
+		boolean fileInput = true;
+		InputStream inputStream = fileInput ? new FileInputStream("input.txt") : System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+
+		Task task = new Task();
+		task.solve(in, out);
+
+		out.close();
+	}
+
+	static class Task {
+		public void solve(InputReader in, PrintWriter out) {
+			// Logic goes here
+			int n = in.nextInt();
+			int[] data = in.nextIntArray(n);
+			List<Integer> ret = new ArrayList<>();
+			Arrays.sort(data);
+			int minDif = Integer.MAX_VALUE;
+			for (int i = 0; i < n - 1; ++i) {
+				int dif = Math.abs(data[i] - data[i + 1]);
+				if (minDif > dif) {
+					ret.clear();
+					ret.add(data[i]);
+					ret.add(data[i + 1]);
+					minDif = dif;
+				} else if (minDif == dif) {
+					ret.add(data[i]);
+					ret.add(data[i + 1]);
+				}
+			}
+
+			for (int it : ret) {
+				out.print(it + " ");
+			}
+			out.println();
+		}
+	}
+
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
+
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		public float nextFloat() {
+			return Float.parseFloat(next());
+		}
+
+		public int[] nextIntArray(int size) {
+			int[] ret = new int[size];
+			for (int i = 0; i < size; ++i) {
+				ret[i] = nextInt();
+			}
+
+			return ret;
+		}
+
+		public long[] nextLongArray(int size) {
+			long[] ret = new long[size];
+			for (int i = 0; i < size; ++i) {
+				ret[i] = nextLong();
+			}
+
+			return ret;
+		}
+
+		public float[] nextFloatArray(int size) {
+			float[] ret = new float[size];
+			for (int i = 0; i < size; ++i) {
+				ret[i] = nextFloat();
+			}
+
+			return ret;
+		}
+
+		public double[] nextDoubleArray(int size) {
+			double[] ret = new double[size];
+			for (int i = 0; i < size; ++i) {
+				ret[i] = nextFloat();
+			}
+
+			return ret;
+		}
+	}	
+}
